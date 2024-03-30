c6892f6a10e2127e01b3279746d477c2bbaa38eb
yoshihiro-imai <yoshihiro.imai29@gmail.com>
2024-03-31 01:29:21 +0900

    test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+
+        // test
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
